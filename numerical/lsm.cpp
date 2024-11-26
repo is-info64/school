8f9bdd441fded3455fbbd53cfd611cbc7720690e
is-info64 <133745029+is-info64@users.noreply.github.com>
2024-11-26 14:18:30 +0400

    Update lsm.cpp

--- numerical/lsm.cpp
+++ numerical/lsm.cpp
@@ -3,21 +3,21 @@
 
 void TranjectoryModel(double a_0, double v_0, double x_end, double N);
 
-int main(){
+int main() {
     double a_0 = 3.14 / 6.0;
-    double v_0 = 10.0;
+    double v_0 = 20.0;
     double N = 50;
-    
-    TranjectoryModel(a_0, v_0, N);
+
+    TranjectoryModel(a_0, v_0, 35, N);
 }
 
-void TranjectoryModel(double a_0, double v_0, double x_end, double N){
-        std::cout << N << std::endl;
-        double y = 0;
-        double x = 0;
-        double step = x_end / N;
-        for(double x = 0; x < x_end; x += step){
-            y = x * tan(a_0) - (9.8 * (x*x)) / (2*(v_0*v_0) * (cos(a_0)*cos(a_0)));
-            std::cout << x << "\t" << y << std::endl;
-        }   
+void TranjectoryModel(double a_0, double v_0, double x_end, double N) {
+    std::cout << N << std::endl;
+    double y = 0;
+    double x = 0;
+    double step = x_end / N;
+    for (double x = 0; x < x_end; x += step) {
+        y = x * tan(a_0) - (9.8 * (x * x)) / (2 * (v_0 * v_0) * (cos(a_0) * cos(a_0)));
+        std::cout << x << "\t" << y << std::endl;
     }
+}
