c23e448a82d7e4ec400b2fdf379ae0588130afdd
is-info64 <133745029+is-info64@users.noreply.github.com>
2024-11-26 14:14:39 +0400

    Update lsm.cpp

--- numerical/lsm.cpp
+++ numerical/lsm.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-void TranjectoryModel(double a_0, double v_0, double N);
+void TranjectoryModel(double a_0, double v_0, double x_end, double N);
 
 int main(){
     double a_0 = 3.14 / 6.0;
@@ -11,19 +11,13 @@ int main(){
     TranjectoryModel(a_0, v_0, N);
 }
 
-void TranjectoryModel(double a_0, double v_0, double N){
+void TranjectoryModel(double a_0, double v_0, double x_end, double N){
         std::cout << N << std::endl;
         double y = 0;
         double x = 0;
-        double step = 30 / N;
-        for(int i = 0; i < N; ++i){
-            x = i * N;
+        double step = x_end / N;
+        for(double x = 0; x < x_end; x += step){
             y = x * tan(a_0) - (9.8 * (x*x)) / (2*(v_0*v_0) * (cos(a_0)*cos(a_0)));
             std::cout << x << "\t" << y << std::endl;
-            if(x == 30){
-                break;
-            }
-            
         }   
-        
     }
